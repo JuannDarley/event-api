831bc4fdeba59c841d8f063dcc887147b1db95d4
JuannDarley <juandarley99@gmail.com>
2025-03-12 17:28:13 -0300

    update variables ambient and validate

--- src/env.ts
+++ src/env.ts
@@ -1,8 +1,9 @@
 import { z } from 'zod'
+import 'dotenv/config'
 
 const envSchema = z.object({
   PORT: z.coerce.number().default(3333),
-  STRING_CONECTION: z.string().url(),
+  STRING_CONECTION: z.string(),
 })
 
 export const env = envSchema.parse(process.env)
